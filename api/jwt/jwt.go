a574a257ee32ff99cd9bdf8bc30dccc06e12d372
simlecode <hj1557986@gmail.com>
2021-07-15 13:17:56 +0800

    forbid send funds

--- api/jwt/jwt.go
+++ api/jwt/jwt.go
@@ -5,17 +5,19 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/venus-messager/config"
+	"github.com/filecoin-project/venus-messager/log"
 )
 
 type JwtClient struct {
 	Local, Remote jwtclient.IJwtAuthClient
 }
 
-func NewJwtClient(jwtCfg *config.JWTConfig) (*JwtClient, error) {
+func NewJwtClient(log *log.Logger, jwtCfg *config.JWTConfig) (*JwtClient, error) {
 	var err error
 	jc := &JwtClient{
 		Remote: newRemoteJwtClient(jwtCfg),
 	}
+	log.Infof("auth url: %s", jwtCfg.AuthURL)
 	if jc.Local, err = newLocalJWTClient(jwtCfg); err != nil {
 		return nil, xerrors.Errorf("new local jwt client failed %v", err)
 	}
